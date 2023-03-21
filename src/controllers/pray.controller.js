4ffc09fdf9b9648446811882e2d84efdc6e5c281
ihorkompaniiets <igor13kompaniets@gmail.com>
2023-03-21 15:26:01 +0100

    Endpoint fixes

--- src/controllers/pray.controller.js
+++ src/controllers/pray.controller.js
@@ -3,22 +3,64 @@ const config = require('../config');
 const prayTimes = require('../utils/PrayTimes');
 
 const getPrayTime = (async (req, res) => {
-    const { lat, lng, dst, gmt } = req.query;
+    const { lat, lng, dst, gmt, method, format } = req.query;
     let times;
-
 
     try {
         const response = await fetch(`${config.TIME_API_URL}?latitude=${lat}&longitude=${lng}`)
         const data = await response.json();
         const { dateTime } = data;
 
-        times = prayTimes.getTimes(dateTime, [lat, lng], gmt, dst);
+        prayTimes.setMethod(method);
+
+        if(method === 'month') {
+            times = getMonth(dateTime, lat, lng, gmt, dst, format);
+        } else if(method === 'year') {
+            times = getYear(2023, lat, lng, gmt, dst, format);
+        } else {
+            times = prayTimes.getTimes(dateTime, [lat, lng], gmt, dst, format);
+        }
+
     } catch (err) {
         console.error(err);
     }
 
     res.json(times);
 });
+
+function getMonth(date, lat, lng, gmt, dst, timeFormat) {
+    const times = [];
+    const currentDate = new Date(date);
+
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+    const startDate = new Date(year, month, 1);
+    const endDate = new Date(year, month+ 1, 1);
+    const format = timeFormat ? '12hNS' : '24h';
+
+    while (startDate < endDate) {
+        const time = prayTimes.getTimes(startDate, [lat, lng], gmt, dst, format);
+        times.push(time);
+        startDate.setDate(startDate.getDate() + 1);  // next day
+    }
+
+    return times;
+}
+
+function getYear(year, lat, lng, gmt, dst, timeFormat) {
+    const times = [];
+
+    const startDate = new Date(year, 0, 1);
+    const endDate = new Date(year + 1, 0, 1);
+
+    while (startDate < endDate) {
+        const time = prayTimes.getTimes(startDate, [lat, lng], gmt, dst, timeFormat);
+        times.push(time);
+        startDate.setDate(startDate.getDate()+ 1);  // next day
+    }
+
+    return times;
+}
 
 module.exports = {
     getPrayTime
--- src/validations/location.validation.js
+++ src/validations/location.validation.js
@@ -5,7 +5,9 @@ const locationSchema = {
         lat: Joi.number().required(),
         lng: Joi.number().required(),
         dst: Joi.string().required(),
-        gmt: Joi.number().required()
+        gmt: Joi.number().required(),
+        method: Joi.string().valid('year', 'month'),
+        format: Joi.number().valid(12, 24)
     })
 };
 
