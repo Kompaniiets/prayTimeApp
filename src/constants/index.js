f01eb32e78fc2db47d7798a6d3364d97bccad168
ihorkompaniiets <igor13kompaniets@gmail.com>
2023-03-22 19:46:11 +0100

    Refactoring

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    period: {
+        MONTH: 'month',
+        YEAR: 'year'
+    },
+    method: {
+        MWL: 'MWL',
+        ISNA: 'ISNA',
+        EGYPT: 'Egypt',
+        MAKKAH: 'Makkah',
+        KARACHI: 'Karachi',
+        TEHRAN: 'Tehran',
+        JAFARI: 'Jafari'
+    },
+    timeFormat: {
+        H: 'h',
+        HH: 'hh'
+    }
+};
--- src/validations/location.validation.js
+++ src/validations/location.validation.js
@@ -1,13 +1,24 @@
 const Joi = require('joi');
+const { period, method, timeFormat } = require('../constants');
 
 const locationSchema = {
     query: Joi.object().keys({
         lat: Joi.number().required(),
         lng: Joi.number().required(),
-        dst: Joi.string().required(),
-        gmt: Joi.number().required(),
-        method: Joi.string().valid('year', 'month'),
-        format: Joi.number().valid(12, 24)
+        dst: Joi.string(),
+        gmt: Joi.number(),
+        period: Joi.string().valid(period.YEAR, period.MONTH).required(),
+        method: Joi.string().valid(
+            method.MWL,
+            method.ISNA,
+            method.EGYPT,
+            method.MAKKAH,
+            method.KARACHI,
+            method.TEHRAN,
+            method.JAFARI
+        ),
+        format: Joi.string().valid(timeFormat.H, timeFormat.HH),
+        year: Joi.number().min(1900).max(2100)
     })
 };
 
